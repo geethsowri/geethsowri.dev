0780e61e39cb8f78af2828d9590595362931ee67
geethsowri <nainalageethsowri@gmail.com>
2025-05-15 13:57:44 +0530

    analytics enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Portfolio from './components/Portfolio'
+import { Analytics } from "@vercel/analytics/react";
+import React from "react";
+import Portfolio from "./components/Portfolio";
 
 const App = () => {
   return (
     <div>
       <Portfolio />
+      <Analytics />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
