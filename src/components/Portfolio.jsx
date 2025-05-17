cf6024777397b992d613248a3cda0526574d1ba3
geethsowri <nainalageethsowri@gmail.com>
2025-05-17 18:59:01 +0530

    responsive

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,43 +3,85 @@ import "./Portfolio.css";
 const Portfolio = () => {
   return (
     <div>
-      <div class="container">
-        <header class="header">
-          <div class="logo">
-            GEETHSOWRI<span>/</span>DEV
-          </div>
-          <nav class="nav">
-            <a class="active" href="#">
-              home
-            </a>
-            <a class="non-clickale" href="#">
-              my work
-            </a>
-            <a class="non-clickale" href="#">
-              collaborate
-            </a>
-            <div class="nav-right">
-              <a href="https://leetcode.com/u/an_astronaut">leetcode</a>
-              <a href="https://www.codechef.com/users/an_astronaut">codechef</a>
+      {/* Desktop Version */}
+      <div className="desktop-page">
+        <div className="container">
+          <header className="header">
+            <div className="logo">
+              GEETHSOWRI <span>/</span>DEV
             </div>
-            <div class="nav-right">
-              <a href="https://github.com/geethsowri">github</a>
-              <a href="https://linkedin.com/in/geethsowri">linkedin</a>
-              <a href="mailto:nainalageethsowri.com">contact</a>
-            </div>
-          </nav>
-        </header>
+            <nav className="nav">
+              <a className="active" href="#">
+                home
+              </a>
+              <a className="non-clickale" href="#">
+                my work
+              </a>
+              <a className="non-clickale" href="#">
+                collaborate
+              </a>
+              <div className="nav-right">
+                <a href="https://leetcode.com/u/an_astronaut">leetcode</a>
+                <a href="https://www.codechef.com/users/an_astronaut">codechef</a>
+              </div>
+              <div className="nav-right">
+                <a href="https://github.com/geethsowri">github</a>
+                <a href="https://linkedin.com/in/geethsowri">linkedin</a>
+                <a href="mailto:nainalageethsowri.com">contact</a>
+              </div>
+            </nav>
+          </header>
 
-        <main class="content">
-          <div class="message">
-            complete portfolio under development
-            <br />
-            meanwhile, read my articles on
-            <a href="https://medium.com/@geethsowri" className="medium-link">
-              medium
-            </a>
-          </div>
-        </main>
+          <main className="content">
+            <div className="message">
+              complete portfolio under development
+              <br />
+              meanwhile, read my articles on
+              <a href="https://medium.com/@geethsowri" className="medium-link">
+                medium
+              </a>
+            </div>
+          </main>
+        </div>
+      </div>
+
+      {/* Mobile Version */}
+      <div className="mobile-page">
+        <div className="container">
+          <header className="header">
+            <div className="logo">
+              GEETHSOWRI/DEV
+            </div>
+            <nav className="nav">
+              <a className="active" href="#">
+                home
+              </a>
+              <a className="non-clickale" href="#">
+                my work
+              </a>
+              <a className="non-clickale" href="#">
+                collaborate
+              </a>
+              <div className="nav-right">
+                <a href="https://leetcode.com/u/an_astronaut">leetcode</a>
+                <a href="https://www.codechef.com/users/an_astronaut">codechef</a>
+                <a href="https://github.com/geethsowri">github</a>
+                <a href="https://linkedin.com/in/geethsowri">linkedin</a>
+              </div>
+            </nav>
+          </header>
+
+          <main className="content">
+            <div className="message">
+              complete portfolio under development
+              <br />
+              meanwhile, read my articles on
+              <a href="https://medium.com/@geethsowri" className="medium-link">
+                medium
+              </a>
+            </div>
+          </main>
+        </div>
       </div>
     </div>
   );
